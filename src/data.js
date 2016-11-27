316a981f71c832894cc2446fd36eb05446909367
wangdejun <1074201799@qq.com>
2016-11-27 21:31:49 +0800

    add the index.html

---
+++ src/data.js
@@ -0,0 +1,20 @@
+data=[
+    {
+        designer_name:"LOEWE",
+        keywords:"bags",
+        link:null,
+        position:20,
+        product:{liked: 0, durl: "LOEWE", stores: 0},
+        x:0.6237,
+        y:0.8809,
+    },
+    {
+        designer_name:"J.W.ANDERSON",
+        keywords:"Dresses",
+        link:null,
+        position:20,
+        product:{liked: 0, durl: "J.W.ANDERSON", stores: 1},
+        x:0.5401,
+        y:0.3226,
+    }
+]
